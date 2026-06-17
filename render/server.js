fead6f24f8568a4b3d0db8d3c76202c20569f241
KUSHAL KUMAR J <kushalkumarj2006@gmail.com>
2026-06-17 09:02:00 +0530

    Update CORS allowed origin URL

--- render/server.js
+++ render/server.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
     'http://127.0.0.1:3000',
     'http://localhost:5500',
     'http://127.0.0.1:5500',
-    'https://repo-chat-wkqk.onrender.com'
+    'https://colabbridge-jyba.onrender.com'
 ];
 
 const corsOptions = {
